6f988abd30f2fa8223174de1d50cfa4b13b76f89
Donatas Noreika <donoras@gmail.com>
2023-02-07 08:55:11 +0200

    REST 1: aprašytas Post viewas su url

--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post, Comment, PostLike, CommentLike
+from .serializers import PostSerializer
+
 
 # Create your views here.
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer

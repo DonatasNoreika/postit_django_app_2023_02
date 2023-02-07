5409ab1bfc07db0cc7d7d91c9e0e615f993a2b56
Donatas Noreika <donoras@gmail.com>
2023-02-07 08:28:07 +0200

    REST 1: aprašyti modeliai

--- api_example/postit_api/models.py
+++ api_example/postit_api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.CharField(max_length=2000)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=2000)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(to="Comment", on_delete=models.CASCADE)

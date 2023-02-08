1b53ee5ca5951182a4028a1f2c38e63427438dfb
Donatas Noreika <donoras@gmail.com>
2023-02-08 08:26:24 +0200

    REST 2: trinti/redaguoti įrašą gali tik įrašo savininkas

--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Post, Comment, PostLike, CommentLike
 from .serializers import PostSerializer
-
+from rest_framework.exceptions import ValidationError
 
 # Create your views here.
 
@@ -19,4 +19,18 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return super().delete(request, *args, **kwargs)
+        else:
+            raise ValidationError('Negalima trinti svetimų pranešimų!')
+
+    def put(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return super().put(request, *args, **kwargs)
+        else:
+            raise ValidationError('Negalima koreguoti svetimų pranešimų!')
 

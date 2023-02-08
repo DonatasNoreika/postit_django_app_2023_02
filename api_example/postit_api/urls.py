7657072c085946194591c21657e06291de113619
Donatas Noreika <donoras@gmail.com>
2023-02-08 08:43:31 +0200

    REST 2: prie įrašo atvaizduojami tik jo komentarai, juos galima sukurti (user ir post priskiriami automatiškai)

--- api_example/postit_api/urls.py
+++ api_example/postit_api/urls.py
@@ -5,5 +5,5 @@ from .views import PostList, PostDetail, CommentList
 urlpatterns = [
     path('posts', PostList.as_view()),
     path('posts/<int:pk>', PostDetail.as_view()),
-    path('comments', CommentList.as_view()),
+    path('posts/<int:pk>/comments', CommentList.as_view()),
 ]
--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -35,10 +35,15 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
             raise ValidationError('Negalima koreguoti svetimų pranešimų!')
 
 class CommentList(generics.ListCreateAPIView):
-    queryset = Comment.objects.all()
+    # queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def get_queryset(self):
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Comment.objects.filter(post=post)
+
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=self.request.user, post=post)
 
